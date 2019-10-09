66774d920aa9f292c398a20ce108b03086aa1ee8
Dwi Bonggo <dwibonggo25@gmail.com>
2019-10-09 15:34:25 +0700

    Initial commit

---
+++ SimpleAppsWithXcode/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SimpleAppsWithXcode
+//
+//  Created by Dwi Bonggo Pribadi on 10/8/19.
+//  Copyright © 2019 Dwi Bonggo Pribadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var button_login: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: Actions
+   
+    
+}
+
